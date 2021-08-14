5e269c69a9f11386bc3fe9008640cc892739eed2
ayalaluis769 <71358053+ayalaluis769@users.noreply.github.com>
2021-08-14 14:42:26 -0400

    Add files via upload

--- BattleGame/battlegame.py
+++ BattleGame/battlegame.py
@@ -2,14 +2,17 @@ while True:
     wizard = "Wizard"
     elf = "Elf"
     human = "Human"
+    orc = "Orc"
 
     wizard_hp = 70
     elf_hp = 100
     human_hp = 150
+    orc_hp = 115
 
     wizard_damage = 150
     elf_damage = 100
     human_damage = 20
+    orc_damage = 85
 
     dragon_hp = 300
     dragon_damage = 50
@@ -17,35 +20,44 @@ while True:
     print("1) Wizard")
     print("2) Elf")
     print("3) Human")
+    print("4) Orc")
     character  = input("Choose Your Character: ")
+    
 
-    if character == "1":
+
+    if (character == "1") or (character == "Wizard") or (character == "WIZard") or (character == "WIZARD"):
         character = wizard
         my_hp = wizard_hp
         my_damage = wizard_damage
         break
-    if character == "2":
+    if (character == "2") or (character == "Elf") or (character == "eLf") or (character == "ELF"):
         character = elf
         my_hp = elf_hp
         my_damage = elf_damage
         break
-    if character == "3":
+    if (character == "3") or (character == "Human") or (character == "HUmaN") or (character == "HUMAN"):
         character = human
         my_hp = human_hp
         my_damage = human_damage
         break  
-    if character == "4":
-        print("Unknown Character")      
-print("You have chosen the character: ", character ,"\nHealth: ", my_hp, "\nDamage: ", my_damage)  
+    if (character == "4") or (character == "Orc") or (character == "orC") or (character == "ORC"):
+        character = orc
+        my_hp = orc_hp
+        my_damage = orc_damage  
+        break    
+    if character == "Quit":
+        quit()
 
 while True: 
     dragon_hp = dragon_hp - my_damage
+    print("\nThe", character, "damaged the Dragon!", "\n The Dragon's healthpoints are now:", dragon_hp)
+    
     my_hp = my_hp - dragon_damage
-    print("The", character, "damaged the Dragon!", "\n The Dragon's hitpoints are now:", dragon_hp)
-    print("The Dragon strikes back at the ",character, "\n The ",character,"'s hitpoints are now", my_hp)
-    if dragon_hp <= 0:
-        print("The Dragon has lost the battle")
+    print("\nThe Dragon strikes back at the ",character, "\n The ",character,"'s healthpoints are now", my_hp)
+    
+    if my_hp == 0:
+        print("\nThe ", character, "has lost the battle!")
         break
-    if my_hp <= 0:
-        print("The ", character, "has lost the battle!")
+    if dragon_hp == 0:
+        print("\nThe Dragon has lost the battle")
         break
